881fd05b95374cda30d99edb2bd84cfb5b41e6cb
Ilya <admin@vodokanal.local>
2022-01-25 11:14:22 +0300

    feat: add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // str = str.replace(/[\|]/g, '');
+  let stk = [];
+
+  if (str == "||" || str == '()' || str == '[]') {
+    return true;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    let x = str[i];
+
+    if (x == '(' || x == '[' || x == '{') {
+      stk.push(x);
+      continue;
+    }
+
+    if (stk.length == 0)
+      return false;
+
+    let check;
+    switch (x) {
+      case ')':
+        check = stk.pop();
+        if (check == '{' || check == '[')
+          return false;
+        break;
+
+      case '}':
+        check = stk.pop();
+        if (check == '(' || check == '[')
+          return false;
+        break;
+
+      case ']':
+        check = stk.pop();
+        if (check == '(' || check == '{')
+          return false;
+        break;
+    }
+  }
+
+  // Check Empty stk
+  return (stk.length == 0);
 }
